99863842cfec80302f62146e39052237180dee87
PoornaShakthi13 <shakthihw13@gmail.com>
2022-07-12 11:00:20 +0530

    Ijse Intake

---
+++ ijse_intake/src/controller/StudentFormcontroller.java
@@ -0,0 +1,29 @@
+package controller;
+
+import com.jfoenix.controls.*;
+import javafx.event.ActionEvent;
+
+public class StudentFormcontroller {
+    public JFXTextField txtStuentName;
+    public JFXTextField txtAddress;
+    public JFXTextField txtContact;
+    public JFXRadioButton rdoMale;
+    public JFXRadioButton rdoFemale;
+    public JFXButton btnStudentTable;
+    public JFXButton btnDelete;
+    public JFXButton btnUpdate;
+    public JFXComboBox cmbStudent;
+    public JFXDatePicker Date;
+
+    public void rdoGenderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnStudentTableOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
+}
